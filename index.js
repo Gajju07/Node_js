c33076ada8cfe74b9796d4dad0ea037529ed1d00
Gajju07 <arpitkumargajbe@gmail.com>
2026-01-20 09:38:39 +0530

    1 day

---
+++ index.js
@@ -0,0 +1,15 @@
+// Basic Node.js application entry point
+
+console.log('Hello from Node.js!');
+
+// Example: Simple HTTP server (uncomment to use)
+// const http = require('http');
+// 
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World!');
+// });
+// 
+// server.listen(3000, () => {
+//   console.log('Server running on http://localhost:3000');
+// });
