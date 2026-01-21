c12f327d9a68d38fca399407b2378d8682fe4a17
Gajju07 <arpitkumargajbe@gmail.com>
2026-01-21 13:40:42 +0530

    2nd day

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 
 console.log('Hello from Node.js!');
 
+// const https = require('https');
+// 
+// const server = https.createServer((req, res) => {
+//     res.write('Welcome to our first secure server!');
+//     res.end();
+// });
+// 
+// server.listen(5000)
+
 // Example: Simple HTTP server (uncomment to use)
-// const http = require('http');
-// 
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello World!');
-// });
-// 
-// server.listen(3000, () => {
-//   console.log('Server running on http://localhost:3000');
-// });
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
