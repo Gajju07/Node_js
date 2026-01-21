d16ddef710cfda2bd46fbe2562af42a51cc6d9fc
Gajju07 <arpitkumargajbe@gmail.com>
2026-01-21 18:25:24 +0530

    6.26 pm

---
+++ f2.js
@@ -0,0 +1,41 @@
+// Event loop example
+
+console.log('Start');
+
+setTimeout (() => {
+  console.log('Second output and the timer is set to 1 ')
+} , 1);
+
+console.log('End');
+
+// Setting up the server and displaying output
+
+const http = require('http'); // Don't forget to import http!
+
+console.log('Start');
+
+const lineBreak = `<br>`;
+
+const server = http.createServer((req, res) => {
+  console.log('Start');
+
+  // 1. Set the timer inside the request handler
+  setTimeout(() => {
+    console.log('Timer finished! Sending response now.');
+
+    // THE FIX: Tell the browser this is an HTML document
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    // Using your lineBreak variable inside backticks
+    res.write(`Second output and the timer is set to 1 second ${lineBreak}`);
+    res.end('Hello from f2.js after a 1-second delay!');
+  }, 1000);
+
+  console.log('End');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
+
+console.log('End');
---
+++ npm.js
@@ -0,0 +1,2 @@
+// Refer to MS word document for recent edits😊
+// link :- https://docs.google.com/document/d/1yLd7poUE2Mo9wEzBP6Ta4DeqTPsgNvjlXGyj6hu21cw/edit?tab=t.0
